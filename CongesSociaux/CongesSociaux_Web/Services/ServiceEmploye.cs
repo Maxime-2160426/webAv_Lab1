08c82a46c5ce8510c7e73fa1ffa7cd0867926102
Max <2160426@cegepmontpetit.ca>
2023-05-02 08:50:12 -0400

    FCT : IServiceEmploye

---
+++ CongesSociaux/CongesSociaux_Web/Services/ServiceEmploye.cs
@@ -0,0 +1,23 @@
+﻿using CongesSociaux_Web.Models;
+using CongesSociaux_Web.Services.IServices;
+
+namespace CongesSociaux_Web.Services
+{
+    public class ServiceEmploye : IServiceEmploye
+    {
+        public void AddEmploye()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreaBanqueMala(Employe employe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MisAJourBanqueMala()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
